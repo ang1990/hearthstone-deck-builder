c3377c95d5e2ba3f27f97dd4299aa54b5339b566
ang1990 <ang1990@hotmail.com>
2019-01-21 00:25:04 +0800

    Somehow the settings file got changed. fixing...

--- django_server/django_server/settings.py
+++ django_server/django_server/settings.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.static',
+    'django.contrib.staticfiles',
 
     'django_server',
     'deck_builder',
@@ -121,7 +121,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
