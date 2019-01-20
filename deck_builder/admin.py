b5503ce36d624ab95043735b647c4baeb90d476d
ang1990 <ang1990@hotmail.com>
2019-01-21 03:34:55 +0800

    Added admin.

--- deck_builder/admin.py
+++ deck_builder/admin.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from .models import HearthstoneCard
 
 from django.contrib import admin
 
 # Register your models here.
+
+admin.register(HearthstoneCard)
